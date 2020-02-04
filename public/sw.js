18532a8526e0ac052ca166cfc6e76e4f489f3e5b
oussamaAlouat <al317257@uji.es>
2020-02-04 12:58:26 +0100

    Added eventListener to fetch event

--- public/sw.js
+++ public/sw.js
@@ -12,3 +12,7 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
+self.addEventListener('fetch', function (event) {
+  console.log('[ServiceWorker] Fetching something ...', event);
+  event.respondWith(fetch(event.request));
+});
