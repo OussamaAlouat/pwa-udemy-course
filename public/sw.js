4453db5c5959aa6e622a3f42d174a6de8974ee8d
oussamaAlouat <al317257@uji.es>
2020-02-04 12:47:36 +0100

    Added eventListeners to install and activate events

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable prefer-arrow-callback */
+
+self.addEventListener('install', function (event) {
+  console.log('[ServiceWorker] Installing service worker ...', event);
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[ServiceWorker] Activate service worker ...', event);
+  return self.clients.claim();
+});
+
