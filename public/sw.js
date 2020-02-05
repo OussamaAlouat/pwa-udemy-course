562b6a1a66263801f6ef3cc578eb309a35f55e7a
oussamaAlouat <al317257@uji.es>
2020-02-05 13:13:30 +0100

    Added basic cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,27 @@
 self.addEventListener('install', function (event) {
   console.log('[ServiceWorker] Installing service worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[ServiceWorker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/js/material.min.js.map',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+      })
+  );
 });
 
 self.addEventListener('activate', function (event) {
@@ -9,5 +31,15 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
   console.log('[ServiceWorker] Fetching something ...', event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(resp) {
+        if (resp) {
+          return resp;
+        }
+
+        return fetch(event.request);
+      })
+  );
+
 });
